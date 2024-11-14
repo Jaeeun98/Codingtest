707467a77717738033556dc1c8384d7396ac33e2
kwon Jaeeun <58484758+Jaeeun98@users.noreply.github.com>
2024-11-14 16:58:07 +0900

    ✨ feat : [백준] 26042 식당 입구 대기 줄

---
+++ acmicpc/26042/index.js
@@ -0,0 +1,45 @@
+//대기하는 학생 수가 최대인 순간의 학생 수와 이때 식당 입구 맨 뒤에 대기 중인 학생의 번호를 출력
+//최대인 경우가 여러 번이라면 맨 뒤에 줄서 있는 학생의 번호가 가장 작은 경우 출력
+
+/**
+ * 첫 번째 = n = 반복 횟수
+ * 두 번째부터 = A = 정보
+ * A는 두 가지 유형이 존재
+ * > 1. 학생 1명이 학교 식당에 도착해 입구 맨 뒤에 줄을 선다
+ * > 2. 맨 앞 학생 1명이 식사를 한다.
+ * a = 학생번호
+ * 최종
+ * - A[0] = 유형번호
+ * - A[1] = 학생 번호
+ */
+
+const fs = require("fs");
+const input = fs.readFileSync("./index.txt").toString().trim().split("\n");
+
+const arr = input.slice(1);
+const waitLine = [];
+let maxLen = 0;
+let lastStudentNumber = -1;
+
+/**
+ * type = 1이면, 학생번호를 waitingLine에 추가
+ * type = 2면, waitingLine 맨 앞 빼기
+ * 한 줄이 끝날때마다
+ */
+
+arr.forEach((A) => {
+  const [type, number] = A.split(" ");
+
+  if (type == 1) waitLine.push(number);
+  else if (waitLine.length > 0) waitLine.shift();
+
+  const waitLen = waitLine.length;
+
+  if (waitLen > maxLen) {
+    maxLen = waitLen;
+    lastStudentNumber = waitLine[waitLen - 1];
+  } else if (waitLen === maxLen && waitLine[waitLen - 1] < lastStudentNumber)
+    lastStudentNumber = waitLine[waitLen - 1];
+});
+
+console.log(maxLen, lastStudentNumber);
